6034d63cf976d1e80809792844c28291b1c3e4d1
Xavier Arteaga <xavie.arteaga@gmail.com>
2026-06-24 13:53:21 +0200

    Move CUDA LDPC decoder to an interface and implementation

--- include/ocuda-fec/phy/upper/channel_coding/ldpc_decoder_cuda.h
+++ include/ocuda-fec/phy/upper/channel_coding/ldpc_decoder_cuda.h
@@ -3,33 +3,27 @@
 // Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
 
 /// \file
-/// \brief LDPC decoder declaration - generic implementation.
+/// \brief LDPC decoder interface - CUDA backend.
 
 #pragma once
 
-#include "ldpc_decoder_cuda_backend.h"
-#include "ldpc_graph_impl.h"
+#include "ocuda-fec/phy/upper/channel_coding/ldpc_decoder_cuda_backend.h"
 #include "ocudu/adt/bit_buffer.h"
+#include "ocudu/phy/upper/channel_coding/ldpc/ldpc.h"
 #include "ocudu/phy/upper/log_likelihood_ratio.h"
+#include "ocudu/ran/sch/ldpc_base_graph.h"
 #include "ocudu/ran/sch/modulation_scheme.h"
+#include <memory>
 
 namespace ocudu {
 
-/// Generic LDPC decoder implementation without any optimization.
+class cuda_ldpc_decoder_backend;
+
+/// LDPC decoder interface - CUDA backend.
 class ldpc_decoder_cuda
 {
 public:
-  /// Maximum number of information bits in a codeblock (before shortening).
-  static constexpr unsigned MAX_BG_K = 22;
-
-  /// \brief Maximum degree of a check node.
-  ///
-  /// In the base graph, each check node is connected, at most, to all variable nodes in the high-rate region
-  /// (of maximum length max_BG_K + 4) and an extra variable node in the extension region.
-  static constexpr unsigned MAX_CHECK_NODE_DEGREE = MAX_BG_K + 5;
-
-  /// Constructor: configures the force_decoding flag.
-  ldpc_decoder_cuda(std::shared_ptr<cuda_ldpc_decoder_backend> backend_);
+  virtual ~ldpc_decoder_cuda() = default;
 
   /// Decoder configuration.
   struct configuration {
@@ -53,29 +47,21 @@ public:
     unsigned Nref;
   };
 
-  // See the ldpc_decoder interface for the documentation.
-  void decode(bit_buffer&                       output,
-              span<const log_likelihood_ratio>  input,
-              const configuration&              cfg,
-              cuda_ldpc_decoder_callback_func&& callback);
-
-private:
-  std::shared_ptr<cuda_ldpc_decoder_backend> cuda_backend;
-  /// Number of significant (not filler) bits.
-  uint16_t nof_significant_bits = 44;
-
-  /// Buffer to store the current value of the soft bits.
-  cuda::device_vector<int8_t, static_cast<size_t>(ldpc::MAX_BG_N_FULL* ldpc::MAX_LIFTING_SIZE)> d_input;
-
-  /// Buffer to store the current value of the soft bits.
-  cuda::device_vector<int8_t, static_cast<size_t>(ldpc::MAX_BG_N_FULL* ldpc::MAX_LIFTING_SIZE)> d_soft_bits;
-
-  /// Buffer to store the current value of the check-to-variable messages.
-  cuda::device_vector<int8_t, static_cast<size_t>(MAX_CHECK_NODE_DEGREE* ldpc::MAX_LIFTING_SIZE* ldpc::MAX_BG_M)>
-      d_check_to_var;
-
-  /// Temporal rate matching buffer.
-  cuda::device_vector<int8_t, static_cast<size_t>(ldpc::MAX_BG_N_FULL* ldpc::MAX_LIFTING_SIZE)> d_rm_buffer;
+  /// \brief Decode a single code block.
+  ///
+  /// \param[out] output   Bit buffer for the decoded code block.
+  /// \param[in]  input    LLRs for the code block.
+  /// \param[in]  cfg      LDPC decoder configuration.
+  /// \param[in]  callback Completion callback.
+  virtual void decode(bit_buffer&                       output,
+                      span<const log_likelihood_ratio>  input,
+                      const configuration&              cfg,
+                      cuda_ldpc_decoder_callback_func&& callback) = 0;
 };
+
+/// \brief Create an LDPC decoder for a CUDA backend.
+///
+/// \param[in] backend  CUDA decoder backend.
+std::unique_ptr<ldpc_decoder_cuda> create_ldpc_decoder_cuda(std::shared_ptr<cuda_ldpc_decoder_backend> backend);
 
 } // namespace ocudu
